92a6ff662d8daa636a4b84395f932d546b2376c8
bangagung <56137774+bangagung@users.noreply.github.com>
2020-12-26 22:55:22 +0700

    Update compilekernel.sh

--- termux/compilekernel.sh
+++ termux/compilekernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export KBUILD_BUILD_USER=xyzuan
-export KBUILD_BUILD_HOST=xyzscape
+export KBUILD_BUILD_USER=Belegung
+export KBUILD_BUILD_HOST=Belegung-Project
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig
+    make -j$(nproc) O=out ARCH=arm64 belegung_defconfig
     make -j$(nproc) ARCH=arm64 O=out \
                                CROSS_COMPILE=aarch64-linux-gnu- \
                                CROSS_COMPILE_ARM32=arm-linux-gnueabi-
